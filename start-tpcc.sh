50a3fdd1d8ddb3ffb9c8e74575aa04092c98fa5c
Changjun <icedance2021@gmail.com>
2025-05-23 17:05:14 +0000

    Add project build option in tpcc script.

--- start-tpcc.sh
+++ start-tpcc.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Check for -b or --build parameter
+build=false
+for arg in "$@"; do
+  case $arg in
+    -b|--build)
+      build=true
+      shift # Remove --build from processing
+      ;;
+  esac
+done
+
+# Run compilation if build is true
+if [ "$build" = true ]; then
+  echo "Running project compilation..."
+  mvn clean install -DskipTests=true
+fi
+
 start_process() {
     local jar_path=$1
     local server=$2
@@ -15,7 +32,9 @@ start_process "tpcc/inventory-tpcc/target/inventory-tpcc-1.0-SNAPSHOT-jar-with-d
 start_process "tpcc/order-tpcc/target/order-tpcc-1.0-SNAPSHOT-jar-with-dependencies.jar" "order"
 start_process "tpcc/warehouse-tpcc/target/warehouse-tpcc-1.0-SNAPSHOT-jar-with-dependencies.jar" "warehouse"
 
-sleep 3
+seconds=7
+echo "Waiting for $seconds seconds..."
+sleep $seconds
 
 echo "Starting proxy server..."
 java --enable-preview \
